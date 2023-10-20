d701678e9ff231f293ccb4a8d0d2e9d09dbdcdca
OPONE <oponechukwuyenum@gmail.com>
2023-10-20 15:37:00 +0100

    Init

---
+++ ThreeGPStart/Renderer.cpp
@@ -0,0 +1,261 @@
+#include "Renderer.h"
+#include "Camera.h"
+#include "ImageLoader.h"
+
+Renderer::Renderer() 
+{
+
+}
+
+// On exit must clean up any OpenGL resources e.g. the program, the buffers
+Renderer::~Renderer()
+{
+	// TODO: clean up any memory used including OpenGL objects via glDelete* calls
+	glDeleteProgram(m_program);
+	glDeleteBuffers(1, &m_VAO);
+}
+
+// Use IMGUI for a simple on screen GUI
+void Renderer::DefineGUI()
+{
+	// Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	{
+		ImGui::Begin("RTG");                    // Create a window called "RTG" and append into it.
+
+		ImGui::Text("Visibility.");             // Display some text (you can use a format strings too)	
+
+		ImGui::Checkbox("Wireframe", &m_wireframe);	// A checkbox linked to a member variable
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		
+		ImGui::End();
+	}
+}
+
+// Load, compile and link the shaders and create a program object to host them
+bool Renderer::CreateProgram()
+{
+	// Create a new program (returns a unqiue id)
+	m_program = glCreateProgram();
+
+	// Load and create vertex and fragment shaders
+	GLuint vertex_shader{ Helpers::LoadAndCompileShader(GL_VERTEX_SHADER, "Data/Shaders/vertex_shader.glsl") };
+	GLuint fragment_shader{ Helpers::LoadAndCompileShader(GL_FRAGMENT_SHADER, "Data/Shaders/fragment_shader.glsl") };
+	if (vertex_shader == 0 || fragment_shader == 0)
+		return false;
+
+	// Attach the vertex shader to this program (copies it)
+	glAttachShader(m_program, vertex_shader);
+
+	// The attibute 0 maps to the input stream "vertex_position" in the vertex shader
+	// Not needed if you use (location=0) in the vertex shader itself
+	//glBindAttribLocation(m_program, 0, "vertex_position");
+
+	// Attach the fragment shader (copies it)
+	glAttachShader(m_program, fragment_shader);
+
+	// Done with the originals of these as we have made copies
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	// Link the shaders, checking for errors
+	if (!Helpers::LinkProgramShaders(m_program))
+		return false;
+
+	return !Helpers::CheckForGLError();
+}
+
+// Load / create geometry into OpenGL buffers	
+bool Renderer::InitialiseGeometry()
+{
+	// Load and compile shaders into m_program
+	if (!CreateProgram())
+		return false;
+
+	// Helpers has an object for loading 3D geometry, supports most types
+	
+	// Load in the jeep
+	Helpers::ModelLoader loader;
+	Helpers::ImageLoader imageLoader;
+
+	
+	if (!loader.LoadFromFile("Data\\Models\\Jeep\\jeep.obj"))
+		return false;
+
+	// Todo: Load Image Texture
+	if (!imageLoader.Load("Data\\Models\\Jeep\\jeep_army.jpg"))
+		return false;
+
+
+
+	MeshStruct mymesh;
+	// Now we can loop through all the mesh in the loaded model:
+	for (const Helpers::Mesh& mesh : loader.GetMeshVector())
+	{
+		std::cout << mesh.name << " + MatIndex: " << mesh.materialIndex << "\n";
+
+		mymesh.numElements = mesh.elements.size();
+		/*
+			Create Vertex Buffer Object (VBO) to hold vertex positions
+		*/
+
+		GLuint positionsVBO;
+		glGenBuffers(1, &positionsVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * mesh.vertices.size(), mesh.vertices.data(), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		//TODO: Create another buffer, and send to normals
+		/*
+			Create Normals Buffer Object (VBO) to hold normals positions
+		*/
+		GLuint normalsVB0;
+		glGenBuffers(1, &normalsVB0);
+		glBindBuffer(GL_ARRAY_BUFFER, normalsVB0);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * mesh.normals.size(), mesh.normals.data(), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		/*
+			Create UV COORDS Buffer Object (VBO) to hold normals positions
+		*/
+		GLuint texcoordsVBO;
+		glGenBuffers(1, &texcoordsVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, texcoordsVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * mesh.uvCoords.size(), mesh.uvCoords.data(), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+		GLuint elementsEBO; 
+		glGenBuffers(1, &elementsEBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementsEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * mesh.elements.size(), mesh.elements.data(), GL_STATIC_DRAW); 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+		// TODO: create a VBA to wrap everything and specify locations in the shaders
+
+		// Create a unique id for a vertex array object(VAO)
+		glGenVertexArrays(1, &m_VAO);
+		glBindVertexArray(m_VAO);
+
+		// SET POSITION ATTRIBUTE
+		glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,                  // attribute 0
+			3,                  // size in bytes of each item in the stream
+			GL_FLOAT,           // type of the item
+			GL_FALSE,           // normalized or not (advanced)
+			0,                  // stride (advanced)
+			(void*)0            // array buffer offset (advanced)
+		);
+		// SET NORMAL ATTRIBUTE
+		glBindBuffer(GL_ARRAY_BUFFER, normalsVB0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(
+			1,                  // attribute 0
+			3,                  // size in bytes of each item in the stream
+			GL_FLOAT,           // type of the item
+			GL_FALSE,           // normalized or not (advanced)
+			0,                  // stride (advanced)
+			(void*)0            // array buffer offset (advanced)
+		);
+
+		// SET UV COORDS ATTRIBUTE
+		glBindBuffer(GL_ARRAY_BUFFER, texcoordsVBO);
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(
+			2,                  // attribute 0
+			2,                  // size in bytes of each item in the stream
+			GL_FLOAT,           // type of the item
+			GL_FALSE,           // normalized or not (advanced)
+			0,                  // stride (advanced)
+			(void*)0            // array buffer offset (advanced)
+		);
+		// Elements - Element array buffer is special!
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementsEBO);
+		// Clear VAO binding;
+		glBindVertexArray(0);
+
+
+		m_meshVector.push_back(mymesh);
+	}
+
+	glGenTextures(1, &m_tex);
+	glBindTexture(GL_TEXTURE_2D, m_tex);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageLoader.Width(), imageLoader.Height(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageLoader.GetData());
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+
+
+	// Good idea to check for an error now:	
+	Helpers::CheckForGLError();
+
+	
+	return true;
+}
+
+// Render the scene. Passed the delta time since last called.
+void Renderer::Render(const Helpers::Camera& camera, float deltaTime)
+{			
+
+	// Configure pipeline settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	// Wireframe mode controlled by ImGui
+	if (m_wireframe)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	// Clear buffers from previous frame
+	glClearColor(0.0f, 0.0f, 0.0f, 0.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// TODO: Compute viewport and projection matrix
+	GLint viewportSize[4];
+	glGetIntegerv(GL_VIEWPORT, viewportSize);
+	const float aspect_ratio = viewportSize[2] / (float)viewportSize[3];
+	glm::mat4 projection_xform = glm::perspective(glm::radians(45.0f), aspect_ratio, 1.0f, 4000.0f);
+
+	// TODO: Compute camera view matrix and combine with projection matrix for passing to shader
+	glm::mat4 view_xform = glm::lookAt(camera.GetPosition(), camera.GetPosition() + camera.GetLookVector(), camera.GetUpVector());
+	glm::mat4 combined_xform = projection_xform * view_xform;
+	glUseProgram(m_program);
+
+	// TODO: Send the combined matrix to the shader in a uniform
+	GLuint combined_xform_id = glGetUniformLocation(m_program, "combined_xform");
+	glUniformMatrix4fv(combined_xform_id, 1, GL_FALSE, glm::value_ptr(combined_xform));
+
+
+
+	glm::mat4 model_xform = glm::mat4(1);
+
+	GLuint model_xform_id = glGetUniformLocation(m_program, "model_xform");
+	glUniformMatrix4fv(model_xform_id, 1, GL_FALSE, glm::value_ptr(model_xform));
+
+
+	//glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+
+	// TODO: render each mesh. Send the correct model matrix to the shader in a uniform
+	for (const MeshStruct mesh : m_meshVector)
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, m_tex);
+		glUniform1i(glGetUniformLocation(m_program, "ourTexture"), 0);
+
+		glBindVertexArray(m_VAO);
+		glDrawElements(GL_TRIANGLES, mesh.numElements, GL_UNSIGNED_INT, (void*)0);
+	}
+
+
+
+	//glBindVertexArray(m_VAO);
+	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	// Always a good idea, when debugging at least, to check for GL errors each frame
+	Helpers::CheckForGLError();
+}
+
---
+++ ThreeGPStart/Renderer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ExternalLibraryHeaders.h"
+
+#include "Helper.h"
+#include "Mesh.h"
+#include "Camera.h"
+
+struct MeshStruct
+{
+	GLuint VAO;
+	GLuint numElements;
+	GLuint meshTexture;
+};
+
+class Renderer
+{
+private:
+	// Program object - to host shaders
+	GLuint m_program{ 0 };
+
+	// Vertex Array Object to wrap all render settings
+	GLuint m_VAO{ 0 };
+
+	// Number of elments to use when rendering
+	GLuint m_numElements{ 0 };
+
+	GLuint m_tex{ 0 };
+
+	bool m_wireframe{ false };
+
+	std::vector<MeshStruct> m_meshVector;
+
+public:
+	Renderer();
+	~Renderer();
+	void DefineGUI();
+
+	// Create the program. This is the compiled shaders.
+	bool CreateProgram();
+
+	// Create and / or load geometry, this is like 'level load'
+	bool InitialiseGeometry();
+
+	// Render the scene
+	void Render(const Helpers::Camera& camera, float deltaTime);
+};
+
